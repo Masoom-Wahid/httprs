510e3bdc5fe5307e8c790b5f3f7fbd228dad3f87
Masoom-Wahid <masoom3892@gmail.com>
2024-08-01 10:13:15 +0430

    Handling .txt and images and also listing dirs when there is one

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::net::TcpListener;
 use std::io::prelude::*;
 use std::net::TcpStream;
-use std::fs;
+use std::path::PathBuf;
+use std::{env, fs};
 use anyhow::Result;
 
 
 
 
 /*
-
+    Fix: handle Index out of bounds: the len is 1 but the index is 1 in line 142 when handling TCP protocol
+    Fix: Better File Handling , since when there is no file i am still getting an error
     TODO: Things to add  ->:
           A URI resoource locator
           image support
@@ -22,7 +24,7 @@ use anyhow::Result;
 
 
 fn main() -> Result<()> {
-    const HOST : &str = "127.0.0.1";
+    const HOST : &str = "0.0.0.0";
     const PORT : &str = "8080";
 
     let end_point : String =  format!("{}:{}",HOST,PORT);
@@ -36,7 +38,7 @@ fn main() -> Result<()> {
 
     for stream in listener.incoming(){
         let _stream = stream?;
-        println!("connection established");
+        println!("connection established from {}",_stream.local_addr()?);
         handle_connection(_stream)?;
     }
 
@@ -100,19 +102,19 @@ fn get_content_type(file_name: &str) -> String {
     content_type
 }
 
-fn get_html_for_dir(dir: std::fs::ReadDir) -> String {
+fn get_html_for_dir(dir: std::fs::ReadDir,abs_path : &str) -> String {
     let mut html = String::from("<html><body><h1>Directory Listing</h1><ul>");
 
     for entry in dir {
         match entry {
             Ok(entry) => {
                 let path = entry.path();
-                let file_name = path.file_name().unwrap_or_default().to_string_lossy();
-
+                let file_name = path.file_name().unwrap_or_default().to_str().unwrap_or_default().to_string();
+                let href_path = abs_path.to_string() + "/" +  &file_name;
                 // Generate a link to each file or directory
                 html.push_str(&format!(
                     "<li><a href=\"{}/\">{}</a></li>",
-                    file_name,
+                    href_path,
                     file_name
                 ));
             }
@@ -139,29 +141,45 @@ fn handle_connection(mut stream: TcpStream) -> Result<()> {
     let values: Vec<&str> = request.split("\n").collect(); 
     let method_info: Vec<&str> = values[0].split(" ").collect();
     let method = method_info[0];
-    let uri = method_info[1];
+    let uri = {
+        if method_info.len() == 1{
+            "/"
+        }else{
+            method_info[1]
+        }
+    };
+    // let uri = method_info[1];
 
     
     
-    let path = match uri {
-        "/" => "index.html",
-        _ => uri.strip_prefix("/").unwrap_or(uri),
+    let mut path = match uri {
+        "/" => "/index.html",
+        _ => uri
+        // _ => uri.strip_prefix("/").unwrap_or(uri),
     };
     
-    if path.ends_with("/"){
-        path.strip_suffix("/");
+    if path.len() != 1 && path.ends_with("/"){
+        path = path.strip_suffix("/").unwrap_or(path);
     }
 
-    let meta_data = fs::metadata(&path)?;
+    println!("Your Path is {path}");
 
-    println!("Your path is {}", path);
+    let binding = env::current_dir()?;
+    let curr_dir = binding.to_str().unwrap();
 
-    println!("it is a dir {}",meta_data.is_dir());
+    let new_request_path : PathBuf = PathBuf::from(&(curr_dir.to_owned() + &path));
+    
+
+    let this = new_request_path.is_dir();
+
+    println!("Is Dir {this}");
+    
+    println!("new_request_path {:?}",new_request_path);
 
     let response = {
-        if meta_data.is_dir(){
-            let data = fs::read_dir(path)?;
-            let content = get_html_for_dir(data);
+        if path != "/"  && new_request_path.is_dir(){
+            let data = fs::read_dir(new_request_path.as_path())?;
+            let content = get_html_for_dir(data,&path);
             let content_type = "text/html";
     
             let header = format!(
@@ -175,11 +193,12 @@ fn handle_connection(mut stream: TcpStream) -> Result<()> {
             dir_response.extend_from_slice(&content.as_bytes());
             dir_response 
         }else{
-            match fs::read(path) {
+            match fs::read(new_request_path.as_path()) {
                 Ok(content) => {
                     let content_type = if is_image(path) {
                         "image/jpeg"
                     } else {
+                        // "application/download"
                         "text/html"
                     };
         
@@ -193,7 +212,7 @@ fn handle_connection(mut stream: TcpStream) -> Result<()> {
                     response.extend_from_slice(header.as_bytes());
                     response.extend_from_slice(&content);
                     response
-                }
+                },
                 Err(e) => {
                     let not_found_page = get_404_page();
                     println!("{:?}", e);
