cd92c536b2969beb37c1eea492810ed40488cd43
Masoom-Wahid <masoom3892@gmail.com>
2024-08-01 11:09:21 +0430

    Refactored the main httprs struct

---
+++ src/core/httprs.rs
@@ -0,0 +1,227 @@
+use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::path::PathBuf;
+use std::{env, fs};
+use anyhow::Result;
+use clap::Parser;
+use log::{debug, info, trace, warn};
+
+
+
+pub struct HttpRs{
+    host : String,
+    port : String,
+}
+
+
+
+
+impl HttpRs{
+    pub fn new(
+        host : &str,
+        port : &str,
+    ) -> Self{
+
+
+        Self{
+            host :host.to_string(),
+            port : port.to_string(),
+        }
+    
+    }
+
+
+    fn get_html_for_dir(&self,dir: std::fs::ReadDir,abs_path : &str) -> String {
+        let mut html = String::from("<html><body><h1>Directory Listing</h1><ul>");
+    
+        for entry in dir {
+            match entry {
+                Ok(entry) => {
+                    let path = entry.path();
+                    let file_name = path.file_name().unwrap_or_default().to_str().unwrap_or_default().to_string();
+                    let href_path = abs_path.to_string() + "/" +  &file_name;
+                    // Generate a link to each file or directory
+                    html.push_str(&format!(
+                        "<li><a href=\"{}/\">{}</a></li>",
+                        href_path,
+                        file_name
+                    ));
+                }
+                Err(e) => {
+                    eprintln!("Error reading directory entry: {:?}", e);
+                    // Handle the error (could append an error message to HTML if needed)
+                }
+            }
+        }
+    
+        html.push_str("</ul></body></html>");
+        html
+    }
+    
+    
+    fn get_404_page(&self) -> String{
+        let result : String =  r#"
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>404 Not Found</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    text-align: center;
+                    background-color: #f0f0f0;
+                }
+                h1 {
+                    font-size: 36px;
+                    margin-top: 0;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>404 Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+        </body>
+        </html>
+        "#.to_string();
+
+        result 
+    }
+
+    
+    fn is_image(&self,path : &str) -> bool{
+        let extensions : [&str ; 7] = ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "ico"];
+        path.to_lowercase();
+        for extension in extensions {
+            if path.ends_with(extension) {
+                return true;
+            }
+        }
+        false
+    }
+
+
+    fn handle_connection(&self,mut stream: TcpStream) -> Result<()> {
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer)?;
+    
+        let request = String::from_utf8_lossy(&buffer[..]);
+        println!("Request : {}", request);
+    
+        let values: Vec<&str> = request.split("\n").collect(); 
+        let method_info: Vec<&str> = values[0].split(" ").collect();
+        let method = method_info[0];
+        let uri = {
+            if method_info.len() == 1{
+                "/"
+            }else{
+                method_info[1]
+            }
+        };
+        // let uri = method_info[1];
+    
+        
+        
+        let mut path = match uri {
+            "/" => "/index.html",
+            _ => uri
+            // _ => uri.strip_prefix("/").unwrap_or(uri),
+        };
+        
+        if path.len() != 1 && path.ends_with("/"){
+            path = path.strip_suffix("/").unwrap_or(path);
+        }
+    
+        println!("Your Path is {path}");
+    
+        let binding = env::current_dir()?;
+        let curr_dir = binding.to_str().unwrap();
+    
+        let new_request_path : PathBuf = PathBuf::from(&(curr_dir.to_owned() + &path));
+        
+    
+        let this = new_request_path.is_dir();
+    
+        println!("Is Dir {this}");
+        
+        println!("new_request_path {:?}",new_request_path);
+    
+        let response = {
+            if path != "/"  && new_request_path.is_dir(){
+                let data = fs::read_dir(new_request_path.as_path())?;
+                let content = self.get_html_for_dir(data,&path);
+                let content_type = "text/html";
+        
+                let header = format!(
+                    "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\n\r\n",
+                    content.len(),
+                    content_type
+                );
+        
+                let mut dir_response : Vec<u8> = Vec::new();
+                dir_response.extend_from_slice(header.as_bytes());
+                dir_response.extend_from_slice(&content.as_bytes());
+                dir_response 
+            }else{
+                match fs::read(new_request_path.as_path()) {
+                    Ok(content) => {
+                        let content_type = if self.is_image(path) {
+                            "image/jpeg"
+                        } else {
+                            // "application/download"
+                            "text/html"
+                        };
+            
+                        let header = format!(
+                            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\n\r\n",
+                            content.len(),
+                            content_type
+                        );
+            
+                        let mut response = Vec::new();
+                        response.extend_from_slice(header.as_bytes());
+                        response.extend_from_slice(&content);
+                        response
+                    },
+                    Err(e) => {
+                        let not_found_page = self.get_404_page();
+                        println!("{:?}", e);
+                        let header = format!(
+                            "HTTP/1.1 404 NOT FOUND\r\nContent-Length: {}\r\nContent-Type: text/html\r\n\r\n",
+                            not_found_page.len()
+                        );
+            
+                        let mut response = Vec::new();
+                        response.extend_from_slice(header.as_bytes());
+                        response.extend_from_slice(not_found_page.as_bytes());
+                        response
+                    }
+                }
+            }
+        };
+        
+    
+    
+        stream.write_all(&response)?;
+        stream.flush()?;
+        Ok(())
+    }
+
+
+    pub fn serve(self) -> Result<()> {
+        let end_point : String =  format!("{}:{}",self.host,self.port);
+
+
+        let listener = TcpListener::bind(end_point)?;
+    
+        info!("HttpRs is running on {} and PORT of {}",self.host,self.port);
+        
+        for stream in listener.incoming(){
+            let _stream = stream?;
+            println!("connection established from {}",_stream.local_addr()?);
+            self.handle_connection(_stream)?;
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
-use std::net::TcpListener;
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::path::PathBuf;
-use std::{env, fs};
 use anyhow::Result;
-
-
-
+use clap::Parser;
+use fern;
+use std::{io, time::SystemTime};
+use httprs::core::httprs::HttpRs;
 
 /*
-    Fix: handle Index out of bounds: the len is 1 but the index is 1 in line 142 when handling TCP protocol
-    Fix: Better File Handling , since when there is no file i am still getting an error
     TODO: Things to add  ->:
-          A URI resoource locator
-          image support
-          better file handling
+          Other Methods Support
           more than 1024 bytes for buffer of the request or even a dynamic allocator based on the buffer
           see if u can write TcpListener uself
           and TcpStrea
@@ -23,216 +15,103 @@ use anyhow::Result;
 */
 
 
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args{
+    #[arg(long,default_value="127.0.0.1")]
+    host : String,
+
+    #[arg(long,default_value="8080")]
+    port : String,
+
+    #[arg(short,long,default_value_t=0)]
+    verbose : u64
+}
+
+
+fn setup_logging(verbosity: u64) -> Result<(), fern::InitError> {
+    let mut base_config = fern::Dispatch::new();
+
+    base_config = match verbosity {
+        0 => {
+            // Let's say we depend on something which whose "info" level messages are too
+            // verbose to include in end-user output. If we don't need them,
+            // let's not include them.
+            base_config
+                .level(log::LevelFilter::Info)
+                .level_for("overly-verbose-target", log::LevelFilter::Warn)
+        }
+        1 => base_config
+            .level(log::LevelFilter::Debug)
+            .level_for("overly-verbose-target", log::LevelFilter::Info),
+        2 => base_config.level(log::LevelFilter::Debug),
+        _3_or_more => base_config.level(log::LevelFilter::Trace),
+    };
+
+    // Separate file config so we can include year, month and day in file logs
+    let file_config = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{} {} {}] {}",
+                humantime::format_rfc3339_seconds(SystemTime::now()),
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .chain(fern::log_file("out.log")?);
+
+    let stdout_config = fern::Dispatch::new()
+        .format(|out, message, record| {
+            // special format for debug messages coming from our own crate.
+            if record.level() > log::LevelFilter::Info {
+                out.finish(format_args!(
+                    "DEBUG @ {}: {}",
+                    humantime::format_rfc3339_seconds(SystemTime::now()),
+                    message
+                ))
+            } else {
+                out.finish(format_args!(
+                    "[{} {} {}] {}",
+                    humantime::format_rfc3339_seconds(SystemTime::now()),
+                    record.level(),
+                    record.target(),
+                    message
+                ))
+            }
+        })
+        .chain(io::stdout());
+
+    base_config
+        .chain(file_config)
+        .chain(stdout_config)
+        .apply()?;
+
+    Ok(())
+}
+
+
+
+
 fn main() -> Result<()> {
-    const HOST : &str = "0.0.0.0";
-    const PORT : &str = "8080";
 
-    let end_point : String =  format!("{}:{}",HOST,PORT);
+    let args = Args::parse();
+    
+    let host : &str = &args.host;
+    let port : &str = &args.port;
 
+    setup_logging(args.verbose).expect("Failed To Initialzie logger");
 
-    let listener = TcpListener::bind(end_point)?;
+    let server : HttpRs = HttpRs::new(host,port);
 
-
-    println!("Web server is running on {}",PORT);
-
-
-    for stream in listener.incoming(){
-        let _stream = stream?;
-        println!("connection established from {}",_stream.local_addr()?);
-        handle_connection(_stream)?;
-    }
+    server.serve()?;
 
     Ok(())
 }
 
-fn get_404_page() -> String{
-    let result : String =  r#"
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>404 Not Found</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                text-align: center;
-                background-color: #f0f0f0;
-            }
-            h1 {
-                font-size: 36px;
-                margin-top: 0;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>404 Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-    </body>
-    </html>
-    "#.to_string();
-
-    result 
-}
-
-
-fn is_image(path : &str) -> bool{
-    let extensions : [&str ; 7] = ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "ico"];
-    path.to_lowercase();
-    for extension in extensions {
-        if path.ends_with(extension) {
-            return true;
-        }
-    }
-    false
-}
-
-
-fn get_content_type(file_name: &str) -> String {
-    let mut content_type = String::new();
-
-    match file_name.to_lowercase().as_str() {
-        "jpg" | "jpeg" => content_type.push_str("image/jpeg"),
-        "png" => content_type.push_str("image/png"),
-        "gif" => content_type.push_str("image/gif"),
-        "bmp" => content_type.push_str("image/bmp"),
-        "tiff" => content_type.push_str("image/tiff"),
-        "ico" => content_type.push_str("image/ico"),
-        _ => content_type.push_str("application/octet-stream"),
-    }
-
-    content_type
-}
-
-fn get_html_for_dir(dir: std::fs::ReadDir,abs_path : &str) -> String {
-    let mut html = String::from("<html><body><h1>Directory Listing</h1><ul>");
-
-    for entry in dir {
-        match entry {
-            Ok(entry) => {
-                let path = entry.path();
-                let file_name = path.file_name().unwrap_or_default().to_str().unwrap_or_default().to_string();
-                let href_path = abs_path.to_string() + "/" +  &file_name;
-                // Generate a link to each file or directory
-                html.push_str(&format!(
-                    "<li><a href=\"{}/\">{}</a></li>",
-                    href_path,
-                    file_name
-                ));
-            }
-            Err(e) => {
-                eprintln!("Error reading directory entry: {:?}", e);
-                // Handle the error (could append an error message to HTML if needed)
-            }
-        }
-    }
-
-    html.push_str("</ul></body></html>");
-    html
-}
 
 
 
-fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer)?;
-
-    let request = String::from_utf8_lossy(&buffer[..]);
-    println!("Request : {}", request);
-
-    let values: Vec<&str> = request.split("\n").collect(); 
-    let method_info: Vec<&str> = values[0].split(" ").collect();
-    let method = method_info[0];
-    let uri = {
-        if method_info.len() == 1{
-            "/"
-        }else{
-            method_info[1]
-        }
-    };
-    // let uri = method_info[1];
-
-    
-    
-    let mut path = match uri {
-        "/" => "/index.html",
-        _ => uri
-        // _ => uri.strip_prefix("/").unwrap_or(uri),
-    };
-    
-    if path.len() != 1 && path.ends_with("/"){
-        path = path.strip_suffix("/").unwrap_or(path);
-    }
-
-    println!("Your Path is {path}");
-
-    let binding = env::current_dir()?;
-    let curr_dir = binding.to_str().unwrap();
-
-    let new_request_path : PathBuf = PathBuf::from(&(curr_dir.to_owned() + &path));
-    
-
-    let this = new_request_path.is_dir();
-
-    println!("Is Dir {this}");
-    
-    println!("new_request_path {:?}",new_request_path);
-
-    let response = {
-        if path != "/"  && new_request_path.is_dir(){
-            let data = fs::read_dir(new_request_path.as_path())?;
-            let content = get_html_for_dir(data,&path);
-            let content_type = "text/html";
-    
-            let header = format!(
-                "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\n\r\n",
-                content.len(),
-                content_type
-            );
-    
-            let mut dir_response : Vec<u8> = Vec::new();
-            dir_response.extend_from_slice(header.as_bytes());
-            dir_response.extend_from_slice(&content.as_bytes());
-            dir_response 
-        }else{
-            match fs::read(new_request_path.as_path()) {
-                Ok(content) => {
-                    let content_type = if is_image(path) {
-                        "image/jpeg"
-                    } else {
-                        // "application/download"
-                        "text/html"
-                    };
-        
-                    let header = format!(
-                        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\n\r\n",
-                        content.len(),
-                        content_type
-                    );
-        
-                    let mut response = Vec::new();
-                    response.extend_from_slice(header.as_bytes());
-                    response.extend_from_slice(&content);
-                    response
-                },
-                Err(e) => {
-                    let not_found_page = get_404_page();
-                    println!("{:?}", e);
-                    let header = format!(
-                        "HTTP/1.1 404 NOT FOUND\r\nContent-Length: {}\r\nContent-Type: text/html\r\n\r\n",
-                        not_found_page.len()
-                    );
-        
-                    let mut response = Vec::new();
-                    response.extend_from_slice(header.as_bytes());
-                    response.extend_from_slice(not_found_page.as_bytes());
-                    response
-                }
-            }
-        }
-    };
-    
 
 
-    stream.write_all(&response)?;
-    stream.flush()?;
-    Ok(())
-}
